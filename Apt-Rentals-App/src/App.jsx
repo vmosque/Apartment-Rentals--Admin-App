3661b23156cc0517d78f29f00d84c1a01606304b
Victor Mosqueda <vdmc_123@hotmail.com>
2025-12-11 01:27:38 +0000

    changes

--- Apt-Rentals-App/src/App.jsx
+++ Apt-Rentals-App/src/App.jsx
@@ -4,8 +4,16 @@ import Sidebar from "./components/Sidebar.jsx";
 import Homepage from "./components/Homepage.jsx";
 import Footer from "./components/Footer.jsx";
 import AptData from "../src/assets/listings.json";
+import { useState } from "react";
 
 function App() {
+  const [apt, setApt] = useState(AptData.results);
+
+  const handleDeleteApt = (id) => {
+    const newList = apt.filter((item) => item.id !== id);
+    setApt(newList);
+  };
+
   return (
     <div className="App">
       <div id="primerContainer">
@@ -18,7 +26,7 @@ function App() {
           <Sidebar />
         </div>
         <div className="homepage">
-          <Homepage />
+          <Homepage apt={apt} handleDeleteApt={handleDeleteApt} />
         </div>
       </div>
       <div id="tercerContainer">
--- Apt-Rentals-App/src/components/Homepage.jsx
+++ Apt-Rentals-App/src/components/Homepage.jsx
@@ -1,16 +1,15 @@
-import { useState } from "react";
 import AptData from "../assets/listings.json";
 
-const Homepage = () => {
-  const [apt, setApt] = useState(AptData.results);
+const Homepage = ({ apt, handleDeleteApt }) => {
   return (
     <>
       {apt.slice(1, 6).map((aptCharac) => (
-        <div id="card">
+        <div key={aptCharac.id} id="card">
           <img className="img" src={aptCharac.picture_url} alt="" />
           <h1 className="name">{aptCharac.name}</h1>
           <h1 className="location">{aptCharac.host_location}</h1>
           <h1 className="price">{aptCharac.price}</h1>
+          <button onClick={() => handleDeleteApt(aptCharac.id)}>DELETE</button>
         </div>
       ))}
     </>
